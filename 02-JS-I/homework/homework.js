1ee50c6812aa3d8613af2fe170273cff45ddd8a2
Your Name <silveyra.shyrley@gmail.com>
2021-12-22 16:45:39 -0300

    first commit

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -29,13 +29,16 @@ function devolverString(str) {
   // "Return" la string provista: str
   // Tu código:
   
+  return str;
 }
 
 function suma(x, y) {
   // "x" e "y" son números
   // Suma "x" e "y" juntos y devuelve el valor
   // Tu código:
-  
+//var suma = x+y
+//return suma;
+  return x+y;
 }
 
 function resta(x, y) {
